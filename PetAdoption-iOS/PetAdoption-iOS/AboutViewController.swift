9f6b850852f65c7026af93d2b1e2917905a179c2
Amir Fleminger <amirfl@yahoo.com>
2018-07-03 12:43:00 -0400

    add urls and IBActions

--- PetAdoption-iOS/PetAdoption-iOS/AboutViewController.swift
+++ PetAdoption-iOS/PetAdoption-iOS/AboutViewController.swift
@@ -26,11 +26,24 @@ class AboutViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func twitterTapped(_ sender: UIButton) {
+        openWebpage(address: "https://twitter.com/CodeForOrlando")
+    }
+    
+    @IBAction func rateAppTapped(_ sender: UIButton) {
+        openWebpage(address: "https://itunes.apple.com/us/app/pet-adoptions-cfo/id1356383076?mt=8")
+    }
+    
     @IBAction func codeForOrlandoTapped(_ sender: UIButton) {
         openWebpage(address: "http://www.codefororlando.com")
     }
     
-    @IBAction func sourceCodeTapped(_ sender: Any) {
+    @IBAction func openSourceCodeTapped(_ sender: Any) {
+        openWebpage(address: "https://github.com/cforlando/PetAdoption-iOS/")
+    }
+    
+    @IBAction func openLicensesTapped(_ sender: Any) {
+        openWebpage(address: "https://github.com/cforlando/PetAdoption-iOS/blob/develop/LICENSE.md")
     }
     
     private func openWebpage(address: String) {
