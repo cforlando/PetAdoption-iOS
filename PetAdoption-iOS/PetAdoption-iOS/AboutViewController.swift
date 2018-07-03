1a168a67b6ae3332ca8065305ad41b2a9a6a67b1
Amir Fleminger <amirfl@yahoo.com>
2018-07-03 12:26:55 -0400

    add About page

---
+++ PetAdoption-iOS/PetAdoption-iOS/AboutViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AboutViewController.swift
+//  PetAdoption-iOS
+//
+//  Created by Amir Fleminger on 7/3/18.
+//  Copyright © 2018 Code For Orlando. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func doneTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func codeForOrlandoTapped(_ sender: UIButton) {
+        openWebpage(address: "http://www.codefororlando.com")
+    }
+    
+    private func openWebpage(address: String) {
+        if let url = URL(string: address) {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+    
+}
